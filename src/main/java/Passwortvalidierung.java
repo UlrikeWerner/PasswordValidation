565a9112076859debe7fde1392fc480642345666
UlrikeWerner <ulrike_werner@web.de>
2023-08-28 10:38:02 +0200

    add check for the password length

--- src/main/java/Passwortvalidierung.java
+++ src/main/java/Passwortvalidierung.java
@@ -1,5 +1,10 @@
 public class Passwortvalidierung {
+    static int MIN_PASSWORD_LENGTH = 8;
     public static void main(String[] args) {
 
+    }
+
+    public static boolean hasMinPasswordLength(String password) {
+        return password.length() >= MIN_PASSWORD_LENGTH;
     }
 }
--- src/test/java/PasswortvalidierungTest.java
+++ src/test/java/PasswortvalidierungTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
 public class PasswortvalidierungTest {
+    static int MIN_PASSWORD_LENGTH = 8;
+
+    @Test
+    void returnTrueWhenPasswordIsMinMIN_PASSWORD_LEMGTH(){
+        byte[] array = new byte[MIN_PASSWORD_LENGTH];
+        new Random().nextBytes(array);
+        String password = new String(array, StandardCharsets.UTF_8);
+
+        boolean actual = Passwortvalidierung.hasMinPasswordLength(password);
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void returnFalseWhenPasswordLengthIsUnder8(){
+        byte[] array = new byte[MIN_PASSWORD_LENGTH -2];
+        new Random().nextBytes(array);
+        String password = new String(array, StandardCharsets.UTF_8);
+
+        boolean actual = Passwortvalidierung.hasMinPasswordLength(password);
+        Assertions.assertFalse(actual);
+    }
 }
